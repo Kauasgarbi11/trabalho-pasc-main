533c21f6fe95ff7f960e225dff9800b194f7d518
Kauasgarbi11 <kauasgarbi2008@gmail.com>
2025-05-05 14:49:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,73 +10,273 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('.'));
 
-const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'senac@02',
-    database: '`cheirinho_de_bolo`.`cadastro`'
-});
-
 app.post('/api/mysql', async (req, res) => {
-    const { nome, email, senha, tipo, id } = req.body;
-    try {
-        switch (tipo) {
-            case 'cadastro':
-                var [rows, fields] = await pool.query(
-                    "insert into `cheirinho_de_bolo`.`cadastro` (`nome`, `email`, `senha`) values (?, ?, ?);",
-                    [nome, email, senha]
-                );
-                if (rows.affectedRows > 0) {
-                    res.json({ message: 'Usuário cadastrado com sucesso!' });
-                } else {
-                    throw ('Não foi possível cadastrar o usuário!');
-                }
-                break;
-            case 'login':
-                var [rows, fields] = await pool.query(
-                    "select * from `cheirinho_de_bolo`.`cadastro` where `nome` = ? and `email` = ? and `senha` = ?;",
-                    [nome, email, senha]
-                );
+
+    const { nome, login, senha, tipo, id, domain } = req.body;
+
+    // Aqui estou definindo as variáveis padrão para uso na conexão com o servidor mysql local
+    var srvHost = '127.0.0.1';
+    var srvUser = 'root';
+    var srvPassword = 'senac@02';
+    var srvDatabase = 'cheirinho_de_bolo';
+
+    // Aqui estou definindo as variáveis padrão para uso na conexão com o servidor mysql remoto
+    if (domain != "localhost") {
+        srvHost = 'sql.freedb.tech';
+        srvUser = 'freedb_ gustavocb';
+        srvPassword = 'M237Kd4eBM6s3#y';
+        srvDatabase = 'freedb_cheirinho_de_bolo';
+    }
+
+    const pool = mysql.createPool({
+        host: 'ballast.proxy.rlwy.net',
+        port:13590,
+        user: 'root',
+        password: 'kxkRwgAlcrDMIKyDBhKOSXJVUtFkhPZg',
+        database: 'railway'
+    });
+
+    switch (tipo) {
+        case 'cadastro':
+            var strSql = "";
+            try {
+                strSql = "select * from `railway`.`tbl_login` where `login` = '" + login + "';";
+                var [rows, fields] = await pool.query(strSql);
                 if (rows.length == 1) {
-                    res.json({ message: 'Usuário logado com sucesso' });
+                    res.json({ 
+                        message: 'Login já cadastrado!',
+                        error: 'Favor digitar outro login!'
+                    });
+                    // version 1.0.1: correção de bug que permite cadastrar dois usuários com mesmo login
+                    // depois esta correção se transformará em uma feature (melhoria) que vai ser versionada na próxima versão da feature, ou seja: 
+                    // version: 1.1.0
                 } else {
-                    throw ("Não foi possível logar o usuário!");
+                    var [rows, fields] = await pool.query(
+                        "insert into `railway`.`tbl_login` (`nome`, `login`, `senha`) values ('" + nome + "', '" + login + "', md5('" + senha + "'));"
+                    );
+                    if (rows.affectedRows > 0) {
+                        res.json({ message: 'Usuário cadastrado com sucesso!' });
+                    } else {
+                        throw ('Não foi possível cadastrar o usuário!');
+                    }
                 }
-                break;
-            case 'leitura':
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de cadastro: ${err} ${domain}`,
+                    error: `Erro de cadastro: ${err} ${domain}`
+                });
+            }
+            break;
+        case 'login':
+            var strSql = "";
+            try {
+                strSql = "select * from `" + srvDatabase + "`.`tbl_login` where `login` = '" + login + "' and `senha` = md5('" + senha + "');";
+                var [rows, fields] = await pool.query(strSql);
+                if (rows.length == 1) {
+                    res.json({ 
+                        message: 'Usuário logado com sucesso',
+                        id: rows[0].id
+                    });
+                } else {
+                    throw ("Não foi possível logar o usuário! Cadastro inválido ou duplicado.");
+                }
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de login: ${err}`,
+                    error: `Erro de login: ${err}`
+                });
+            }
+            break;
+        case 'primeiro':
+        case 'leitura':
+            try {
                 var addNome = "";
-                var addEmail = "";
+                var addLogin = "";
                 var addAnd = "";
 
                 if (nome.trim().length > 0) {
                     addNome = " `nome` like '%" + nome + "%' ";
                 }
 
-                if (email.trim().length > 0) {
-                    addEmail = " `email` like '%" + email + "%' ";
+                if (login.trim().length > 0) {
+                    addLogin = " `login` like '%" + login + "%' ";
                 }
 
-                if (nome.trim().length > 0 && email.trim().length > 0) {
+                if (nome.trim().length > 0 && login.trim().length > 0) {
                     addAnd = " and ";
                 }
 
-                var strSql = "select * from `cheirinho_de_bolo`.`cadastro` where" + 
-                    addNome + addAnd + addEmail + ";";
+                var strSql = "select * from `" + srvDatabase + "`.`tbl_login` where" + 
+                    addNome + addAnd + addLogin + " order by `id` asc;";
                 var [rows, fields] = await pool.query(strSql);
                 if (rows.length > 0) {
                     res.json({ 
                         message: 'Nome ou login encontrado com sucesso!',
                         id: rows[0].id,
                         nome: rows[0].nome,
-                        email: rows[0].login,
-                        linhas: rows.length
+                        login: rows[0].login,
+                        linhas: rows
                     });
                 } else {
                     throw ("Não foi possível encontrar o nome ou login!");
                 }
-                break;
-            case 'atualizacao':
-                var strSql = "select * from `cheirinho_de_bolo`.`cadastro`;";
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de leitura: ${err}`,
+                    error: `Erro de leitura: ${err}`
+                });
+            }
+            break;
+        case 'anterior':
+            var strSql = "";
+            try {
+                var addId = "";
+                var addNome = "";
+                var addLogin = "";
+
+                if (typeof(nome) != undefined) {
+                    addNome = " `nome` like '%" + nome + "%' ";
+                }
+
+                if (typeof(login) != undefined) {
+                    addLogin = " `login` like '%" + login + "%' ";
+                }
+
+                if (typeof(nome) != undefined && typeof(login) != undefined) {
+                    addLogin = " and " + addLogin;
+                }
+
+                if (typeof(id) != undefined) {
+                    addId = " `id` < " + id + " ";
+                }
+
+                if (typeof(nome) != undefined && typeof(id) != undefined) {
+                    addId = " and " + addId;
+                } else if (typeof(login) != undefined && typeof(id) != undefined) {
+                    addId = " and " + addId;
+                }
+
+                var strSql = "select * from `" + srvDatabase + "`.`tbl_login` where" + 
+                    addNome + addLogin + addId + " order by `id` desc;";
+                var [rows, fields] = await pool.query(strSql);
+                if (rows.length > 0) {
+                    res.json({
+                        message: 'Nome ou login encontrado com sucesso!',
+                        id: rows[0].id,
+                        nome: rows[0].nome,
+                        login: rows[0].login,
+                        linhas: rows
+                    });
+                } else {
+                    throw ("Não foi possível encontrar o nome ou login!");
+                }
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de anterior: ${err}`,
+                    error: `Erro de anterior: ${err}`
+                });
+            }
+            break;
+        case 'proximo':
+            var strSql = "";
+            try {
+                var addId = "";
+                var addNome = "";
+                var addLogin = "";
+
+                if (typeof(nome) != undefined) {
+                    addNome = " `nome` like '%" + nome + "%' ";
+                }
+
+                if (typeof(login) != undefined) {
+                    addLogin = " `login` like '%" + login + "%' ";
+                }
+
+                if (typeof(nome) != undefined && typeof(login) != undefined) {
+                    addLogin = " and " + addLogin;
+                }
+
+                if (typeof(id) != undefined) {
+                    addId = " `id` > " + id + " ";
+                }
+
+                if (typeof(nome) != undefined && typeof(id) != undefined) {
+                    addId = " and " + addId;
+                } else if (typeof(login) != undefined && typeof(id) != undefined) {
+                    addId = " and " + addId;
+                }
+
+                strSql = "select * from `" + srvDatabase + "`.`tbl_login` where" + 
+                    addNome + addLogin + addId + " order by `id` asc;";
+                var [rows, fields] = await pool.query(strSql);
+                if (rows.length > 0) {
+                    res.json({
+                        message: 'Nome ou login encontrado com sucesso!',
+                        id: rows[0].id,
+                        nome: rows[0].nome,
+                        login: rows[0].login,
+                        linhas: rows
+                    });
+                } else {
+                    // throw ("Não foi possível encontrar o nome ou login!");
+                    throw ("Não foi possível encontrar o nome ou login!");
+                }
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de próximo: ${err}`,
+                    error: `Erro de próximo: ${err}`
+                });
+            }
+            break;
+        case 'ultimo':
+            try {
+                var addNome = "";
+                var addLogin = "";
+                var addAnd = "";
+
+                if (nome.trim().length > 0) {
+                    addNome = " `nome` like '%" + nome + "%' ";
+                }
+
+                if (login.trim().length > 0) {
+                    addLogin = " `login` like '%" + login + "%' ";
+                }
+
+                if (nome.trim().length > 0 && login.trim().length > 0) {
+                    addAnd = " and ";
+                }
+
+                var strSql = "select * from `" + srvDatabase + "`.`tbl_login` where" + 
+                    addNome + addAnd + addLogin + " order by `id` desc;";
+                var [rows, fields] = await pool.query(strSql);
+                if (rows.length > 0) {
+                    res.json({
+                        message: 'Nome ou login encontrado com sucesso!',
+                        id: rows[0].id,
+                        nome: rows[0].nome,
+                        login: rows[0].login,
+                        linhas: rows
+                    });
+                } else {
+                    // throw ("Não foi possível encontrar o nome ou login!");
+                    throw ("Não foi possível encontrar o nome ou login! sql: " + strSql);
+                }
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de último: ${err}`,
+                    error: `Erro de último: ${err}`
+                });
+            }
+            break;
+        case 'atualizacao':
+            try {
+                var strSql = "select * from `" + srvDatabase + "`.`tbl_login` order by `id` asc;";
                 var [rows, fields] = await pool.query(strSql);
                 if (rows.length > 0) {
                     res.json({ 
@@ -86,11 +286,19 @@ app.post('/api/mysql', async (req, res) => {
                 } else {
                     throw ("Não há registro algum na tabela tbl_login!");
                 }
-                break;
-            case 'atualizar':
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de atualização: ${err}`,
+                    error: `Erro de atualização: ${err}`
+                });
+            }
+            break;
+        case 'atualizar':
+            try {
                 var addId = "";
                 var addNome = "";
-                var addEmail = "";
+                var addLogin = "";
                 var addSenha = "";
                 var addAnd = "";
 
@@ -102,24 +310,24 @@ app.post('/api/mysql', async (req, res) => {
                     addNome = " `nome` = '" + nome + "' ";
                 }
 
-                if (email.trim().length > 0) {
-                    addEmail = " `email` = '" + email + "' ";
+                if (login.trim().length > 0) {
+                    addLogin = " `login` = '" + login + "' ";
                 }
 
                 if (addNome.length > 0) {
-                    addEmail = " , " + addEmail;
+                    addLogin = " , " + addLogin;
                 }
 
                 if (senha.trim().length > 0) {
-                    addSenha = " `senha` = '" + senha + "' ";
+                    addSenha = " `senha` = md5('" + senha + "') ";
                 }
 
-                if (addEmail.length > 0) {
+                if (addLogin.length > 0) {
                     addSenha = " , " + addSenha;
                 }
 
-                var strSql = "update `cheirinho_de_bolo`.`cadastro` set " + 
-                    addNome + addEmail + addSenha + 
+                var strSql = "update `" + srvDatabase + "`.`tbl_login` set " + 
+                    addNome + addLogin + addSenha + 
                     " where `id` = " + addId + ";";
                 var [rows, fields] = await pool.query(strSql);
                 if (rows.affectedRows > 0) {
@@ -127,15 +335,43 @@ app.post('/api/mysql', async (req, res) => {
                         message: 'Registro atualizado com sucesso!'
                     });
                 } else {
-                    throw ("Não foi possível atualizar o id: " + addId + " na tabela cadastro!");
+                    throw ("Não foi possível atualizar o id: " + addId + " na tabela tbl_login!");
                 }
-                break;
-            default:
-                throw ("Não foi possível identificar o tipo!");
-        }
-    } catch (err) {
-        // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
-        res.status(500).json({ message: `Erro: ${err}` });
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de atualizar: ${err}`,
+                    error: `Erro de atualizar: ${err}`
+                });
+            }
+            break;
+        case 'remover':
+            try {
+                var addId = "";
+
+                if (id.trim().length > 0) {
+                    addId = id;
+                }
+
+                var strSql = "delete from `" + srvDatabase + "`.`tbl_login` where `id` = " + addId + ";";
+                var [rows, fields] = await pool.query(strSql);
+                if (rows.affectedRows > 0) {
+                    res.json({ 
+                        message: 'Registro removido com sucesso!'
+                    });
+                } else {
+                    throw ("Não foi possível remover o id: " + addId + " na tabela tbl_login!");
+                }
+            } catch (err) {
+                // console.error(err); // aqui não vai aparecer o erro no console, pois este arquivo não é processado pelo frontend, mas sim pelo backend (node server.js)
+                res.status(500).json({ 
+                    message: `Erro de remover: ${err}`,
+                    error: `Erro de remover: ${err}`
+                });
+            }
+            break;
+        default:
+            throw ("Não foi possível identificar o tipo!");
     }
 });
 
